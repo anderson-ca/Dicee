7eff3059ae5789d0991d2ec8237ac9747d4dc376
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-02-06 11:15:43 -0600

    add randomized func to dice 1 and dice 2

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -6,10 +6,33 @@
 //  Copyright © 2018 Anderson Cardoso. All rights reserved.
 //
 
+
+// import functionality from UIKit module
 import UIKit
 
+// ViewController class of annotated type UIViewController.
 class ViewController: UIViewController {
-
+    
+    //////////////////////////////////////
+    //////////////////////////////////////
+    //////////////////////////////////////
+    // variables representing random values to populate left and right dice.
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    // left dice.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    // right dice.
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    // array of Strings
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    
+    //////////////////////////////////////
+    //////////////////////////////////////
+    //////////////////////////////////////
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +43,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //////////////////////////////////////
+    //////////////////////////////////////
+    //////////////////////////////////////
+    
+    
+    // roll dice button. -> this button will function as
+    // a event handler. As the user interacts with this element,
+    // the code inside the block will be executed.
+    @IBAction func UIButton(_ sender: UIButton) {
+        
+        // generate random number between 0 and 6.
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        // programmatically changing properties of elements in the display.
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
 
+        
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
